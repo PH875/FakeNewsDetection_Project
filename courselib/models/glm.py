a7bda8b4a6be7cb749a389c8a72d1649439a61dd
PH_Laptop <philipp.hoffmann.email@gmail.com>
2025-07-10 13:55:21 +0200

    Update courselib

--- courselib/models/glm.py
+++ courselib/models/glm.py
@@ -6,39 +6,45 @@ def sigmoid(x):
 
 class LogisticRegression(TrainableModel):
     """
-    Binary logistic regression model with optional regularization.
+    Binary logistic regression model with optional regularization, adapted for sparse matrix compatibility
 
     Parameters:
         - w: Initial weights (array-like)
         - b: Initial bias (scalar)
         - optimizer: Optimizer object (e.g., GDOptimizer)
         - penalty: One of {"none", "ridge", "lasso"}
-        - alpha: Regularization strength (default: 0.0)
+        - lam: Regularization strength (default: 0.0)
+        - offset: offset of all X in the followin function, the methods give the same result for some X as the corresponding methods
+            in the original LogisticRegression with X-offset. offset is used to make the computation more efficient, useful for z-score normalization of sparse matrices
     """
     
-    def __init__(self, w, b, optimizer, penalty="none", lam=0.0):
+    def __init__(self, w, b, optimizer, penalty="none", lam=0.0, offset=None):
         super().__init__(optimizer)
         self.w = np.array(w, dtype=float)
         self.b = np.array(b, dtype=float)
         self.penalty = penalty
         self.lam = float(lam)
+        if offset is None:
+            self.offset=np.zeros(len(w))
+        else:
+            self.offset=offset
         
 
     def loss_grad(self, X, y):
         residual = self.decision_function(X) - y
-        grad_w = X.T @ residual / len(X)
+        grad_w = (X.T @ residual-np.sum(residual)*self.offset) / X.shape[0]
         grad_b = np.mean(residual)
 
         # Add regularization if specified
         if self.penalty == "ridge":
-            grad_w += self.alpha * self.w
+            grad_w += self.lam * self.w
         elif self.penalty == "lasso":
-            grad_w += self.alpha * np.sign(self.w)
+            grad_w += self.lam * np.sign(self.w)
 
         return {"w": grad_w, "b": grad_b}
     
     def decision_function(self, X):
-        return sigmoid(X @ self.w + self.b)
+        return sigmoid(X @ self.w -np.dot(self.offset,self.w) + self.b)
     
     def _get_params(self):
         """
